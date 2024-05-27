6db8418395d9f9b18356da7bfaaec6c831905b43
DontStopPrograming <01dyamond01@gmail.com>
2024-05-27 13:45:07 -0500

    Services Component - corrected phrase_services v3, About Component - it was done JSX

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,9 +2,29 @@ import './About.css';
 
 export const About = () => {
 	return (
-		<h1>
-			THIS IS ABOUT
-		</h1>
+		<>
+			<div className='container__about'>
+				<div className='container_title_about'>
+					<div> -----------</div>
+					<div className='title_about'> ABOUT </div>
+					<div> -----------</div>
+				</div>
+
+				<div className='phrase_about'>
+					<p> <span> 7 </span> Years of market expertise
+						Providing swift and efficient licensing consultation solutions
+					</p>
+
+					<h1> We're here to ensure you obtain your Operating License and ITSE Certificate hassle-free <br />
+						Civil security guaranteed
+					</h1>
+
+					<p> 100% Completely dedicated </p>
+
+				</div>
+
+			</div>
+		</>
 	);
 };
 
