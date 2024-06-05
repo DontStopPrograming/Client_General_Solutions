074360388c42c565cc02565560e02eba5126d79b
DontStopPrograming <01dyamond01@gmail.com>
2024-06-05 17:22:45 -0500

    Contact Component - completed structure

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,19 +3,21 @@ import './Contact.css';
 export const Contact = () => {
     return (
         <>
-            <div className='container__contact'>
-                <div className='title_contact'>
-                    <h2>
-                        OUR <br /> HIDEAWAY
-                    </h2 >
+            <div className='contact father'>
+                <div className='container__contact'>
+                    <div className='title_contact'>
+                        <h2>
+                            OUR <br /> HIDEAWAY <br /><br />
+                        </h2 >
+                    </div>
+
+                    <div className='sub_location'> <h2> Location </h2><p> Lima, 216 - 217 bla bla bla</p> </div>
+                    <div className='sub_email'> <h2>Email</h2> <p> hello@gmail.com </p></div>
+                    <div className='sub_phone'> <h2>Phone</h2> <p> +51 999 999 999 </p></div>
+                    <div className='sub_follow'> <h2>Follow on</h2> <p> . Facebook . Instagram  </p></div>
+
+
                 </div>
-
-                <div className='sub_location'> <h2> Location </h2><p> Lima, 216 - 217 bla bla bla</p> </div>
-                <div className='sub_email'> <h2>Email</h2> <p> hello@gmail.com </p></div>
-                <div className='sub_phone'> <h2>Phone</h2> <p> +51 999 999 999 </p></div>
-                <div className='sub_follow'> <h2>Follow on</h2> <p> . Facebook . Instagram  </p></div>
-
-
             </div>
 
         </>
