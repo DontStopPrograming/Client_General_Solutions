49e6399f4d33b6afd440cfb6a3a5ef2ff33907b0
DontStopPrograming <01dyamond01@gmail.com>
2024-07-18 18:22:44 -0500

    Fix: Starting animations in all components v5 - denying the Navbar in Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-
-/* The code snippet is importing specific components and modules from different files and libraries. */
-import { Navbar, Home, Services, About, Contact } from './components'
-// import { Animation } from '../src/components/Utils'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
+import { Navbar, Home, Services, About, Contact } from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-
     <Router>
-
-      <Navbar />
-
       <Routes>
-        <Route path='/' element={<Home />}> </Route>
-        <Route path='/home' element={<Home />}> </Route>
-        <Route path='/services' element={<Services />}> </Route>
-        <Route path='/about' element={<About />}> </Route>
-        <Route path='/contact' element={<Contact />}> </Route>
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={
+          <>
+            <Navbar />
+            <Services />
+          </>
+        } />
+        <Route path='/about' element={
+          <>
+            <Navbar />
+            <About />
+          </>
+        } />
+        <Route path='/contact' element={
+          <>
+            <Navbar />
+            <Contact />
+          </>
+        } />
       </Routes>
     </Router>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
